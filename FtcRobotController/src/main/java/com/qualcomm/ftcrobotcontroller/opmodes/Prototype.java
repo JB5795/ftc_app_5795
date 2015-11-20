b615e49817ab70c99e6775eef81935cec9332aae
purihm <hunaid14@gmail.com>
2015-11-20 17:21:01 -0500

    spongemotorservo

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Prototype.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Prototype.java
@@ -14,15 +14,22 @@ public class Prototype extends OpMode {
     DcMotor bright;
     DcMotor fleft;
     DcMotor bleft;
+    DcMotor sponge;
     Servo left;
     Servo right;
     Servo belt;
+    Servo camleft;
+    Servo camright;
 
     double leftPosition;
     double rightPosition;
     double beltPosition;
+    double camleftPosition;
+    double camrightPosition;
     double leftChange=.1;
     double rightChange=.1;
+    double camleftChange=.1;
+    double camrightChange=.1;
     double BELTChange=.1;
     final static double LEFT_MIN_RANGE  = .30;
     final static double LEFT_MAX_RANGE  = 0.95;
@@ -30,17 +37,27 @@ public class Prototype extends OpMode {
     final static double RIGHT_MAX_RANGE  = 0.85;
     final static double BELT_MIN_RANGE  = 0.20;
     final static double BELT_MAX_RANGE  = 0.7;
+    final static double CAMLEFT_MAX_RANGE = .9;
+    final static double CAMLEFT_MIN_RANGE = .3;
+    final static double CAMRIGHT_MAX_RANGE = .9;
+    final static double CAMRIGHT_MIN_RANGE = .3;
+
     public void init (){
         fright = hardwareMap.dcMotor.get("fright");
         bright = hardwareMap.dcMotor.get("bright");
         fleft = hardwareMap.dcMotor.get("fleft");
         bleft = hardwareMap.dcMotor.get("bleft");
+        sponge=hardwareMap.dcMotor.get("sponge");
         left= hardwareMap.servo.get("left");
         right=hardwareMap.servo.get("right");
         belt=hardwareMap.servo.get("belt");
+        camleft=hardwareMap.servo.get ("camleft");
+        camright=hardwareMap.servo.get ("camright");
         leftPosition=.9;
         rightPosition=.1;
         beltPosition=.5;
+        camleftPosition= .3; //edit
+        camrightPosition=.3; //edit
     }
 
     public void loop () {
@@ -48,27 +65,28 @@ public class Prototype extends OpMode {
         bright.setPower(-gamepad1.right_stick_y);
         fleft.setPower(gamepad1.left_stick_y);
         bleft.setPower(gamepad1.left_stick_y);
-        if (gamepad1.dpad_left) {
+        sponge.setPower(gamepad2.left_stick_y);
+        if (gamepad2.dpad_left) {
             leftPosition += leftChange;
         }
 
-        if (gamepad1.dpad_right) {
+        if (gamepad2.dpad_right) {
             leftPosition -= leftChange;
         }
 
-        if (gamepad1.x) {
+        if (gamepad2.x) {
             rightPosition += rightChange;
         }
 
-        if (gamepad1.b) {
+        if (gamepad2.b) {
             rightPosition -= rightChange;
         }
         else{}
-        if (gamepad1.left_bumper){
+        if (gamepad2.left_bumper){
             BELTChange=.1;
             beltPosition += BELTChange;
         }
-        else if (gamepad1.right_bumper){
+        else if (gamepad2.right_bumper){
             BELTChange=.1;
             beltPosition-=BELTChange;
         }
@@ -76,13 +94,25 @@ public class Prototype extends OpMode {
             BELTChange = 0;
             beltPosition=.5;
         }
+        if (gamepad2.y){
+            camleftPosition= camleftPosition + camleftChange;
+            camrightPosition=camrightPosition + camrightChange;
+        }
+        if (gamepad2.a){
+            camleftPosition = camleftPosition - camleftChange;
+            camrightPosition= camrightPosition - camrightChange;
+        }
         leftPosition = Range.clip(leftPosition, LEFT_MIN_RANGE, LEFT_MAX_RANGE);
         rightPosition = Range.clip(rightPosition, RIGHT_MIN_RANGE, RIGHT_MAX_RANGE);
         beltPosition = Range.clip(beltPosition, BELT_MIN_RANGE, BELT_MAX_RANGE);
+        camleftPosition = Range.clip (camleftPosition, CAMLEFT_MIN_RANGE, CAMRIGHT_MAX_RANGE);
+        camrightPosition = Range.clip (camrightPosition, CAMRIGHT_MIN_RANGE, CAMRIGHT_MAX_RANGE);
 
         left.setPosition(leftPosition);
         right.setPosition(rightPosition);
         belt.setPosition(beltPosition);
+        camleft.setPosition (camleftPosition);
+        camright.setPosition(camrightPosition);
 
         telemetry.addData("S Right: ", gamepad1.right_stick_y);
         telemetry.addData("S Left: ", gamepad1.left_stick_y);
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PrototypeAutoTest_2.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PrototypeAutoTest_2.java
@@ -2,6 +2,8 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by jboerger on 11/5/2015.
@@ -11,15 +13,19 @@ public class PrototypeAutoTest_2 extends OpMode {
     DcMotor bright;
     DcMotor fleft;
     DcMotor bleft;
+    Servo left;
+    Servo right;
+    Servo belt;
+    int targetPosition=1120;
 
-//    final static int ENCODER_CPR = 1440;    //encoder counts per revolution
-//    final static double GEAR_RATIO = 2;     //gear ratio
-//    final static int WHEEL_DIAMETER = 4;    //diameter of wheel in inches
-//    static double DISTANCE;                 //distance to drive in inches
-//
-//    final static double CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER; //calculates circumference
-//    final static double ROTATIONS = DISTANCE/CIRCUMFERENCE;       //calculates rotations needed
-//    final static double COUNTS = ENCODER_CPR*ROTATIONS*GEAR_RATIO;//calculates encoder counts needed
+    final static int ENCODER_CPR = 1120;    //encoder counts per revolution
+    final static double GEAR_RATIO = 2;     //gear ratio
+    final static int WHEEL_DIAMETER = 4;    //diameter of wheel in inches
+    static double DISTANCE;                 //distance to drive in inches
+
+    final static double CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER; //calculates circumference
+    final static double ROTATIONS = DISTANCE/CIRCUMFERENCE;       //calculates rotations needed
+    final static double COUNTS = ENCODER_CPR*ROTATIONS*GEAR_RATIO;//calculates encoder counts needed
 
     public void init() {
 
@@ -27,33 +33,29 @@ public class PrototypeAutoTest_2 extends OpMode {
         bright = hardwareMap.dcMotor.get("bright");
         fleft = hardwareMap.dcMotor.get("fleft");
         bleft = hardwareMap.dcMotor.get("bleft");
+        left=hardwareMap.servo.get("left");
+        right=hardwareMap.servo.get("right");
+        belt=hardwareMap.servo.get("belt");
 
-//        fright.setDirection(DcMotor.Direction.REVERSE);
-//        bright.setDirection(DcMotor.Direction.REVERSE);
+        fright.setDirection(DcMotor.Direction.REVERSE);
+        bright.setDirection(DcMotor.Direction.REVERSE);
 
-//        fright.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-//        fleft.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        fright.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+
     }
         public void loop(){                 //While start button is pressed (until stopped)
+            fright.setTargetPosition(targetPosition);
 
-//            fright.setTargetPosition(10000);
-//
-//            fleft.setTargetPosition(10000);
+            if(fright.getCurrentPosition()<targetPosition) {
+                bright.setPower(1);
+                bleft.setPower(1);
+                fright.setPower(1);
+                fleft.setPower(1);
+            }
+            fright.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
 
+            fright.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
 
-//            fright.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
-//
-//            fleft.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
-
-
-            fright.setPower(-1);
-            bright.setPower(-1);
-            fleft.setPower(1);
-            bleft.setPower(1);
-
-//            fright.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-//
-//            fleft.setMode(DcMotorController.RunMode.RESET_ENCODERS);
 
 
 
