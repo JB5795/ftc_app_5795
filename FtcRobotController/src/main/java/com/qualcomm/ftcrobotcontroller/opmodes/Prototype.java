ef2d1ce3a15bf3a5106a0ebba54957c1eec794eb
purihm <hunaid14@gmail.com>
2015-11-20 19:51:35 -0500

    commentoutcam

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Prototype.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Prototype.java
@@ -14,22 +14,22 @@ public class Prototype extends OpMode {
     DcMotor bright;
     DcMotor fleft;
     DcMotor bleft;
-    //DcMotor sponge;
+    DcMotor sponge;
     Servo left;
     Servo right;
     Servo belt;
     Servo camleft;
-    Servo camright;
+    //Servo camright;
 
     double leftPosition;
     double rightPosition;
     double beltPosition;
     double camleftPosition;
-    double camrightPosition;
+    //double camrightPosition;
     double leftChange=.1;
     double rightChange=.1;
     double camleftChange=.1;
-    double camrightChange=.1;
+    //double camrightChange=.1;
     double BELTChange=.1;
     final static double LEFT_MIN_RANGE  = .30;
     final static double LEFT_MAX_RANGE  = 0.95;
@@ -37,29 +37,27 @@ public class Prototype extends OpMode {
     final static double RIGHT_MAX_RANGE  = 0.85;
     final static double BELT_MIN_RANGE  = 0.20;
     final static double BELT_MAX_RANGE  = 0.7;
-    final static double CAMLEFT_MAX_RANGE = .9;
+    final static double CAMLEFT_MAX_RANGE = .5;
     final static double CAMLEFT_MIN_RANGE = 0;
-    final static double CAMRIGHT_MAX_RANGE = 0;
-    final static double CAMRIGHT_MIN_RANGE = .9;
+    //final static double CAMRIGHT_MAX_RANGE = 0;
+    //final static double CAMRIGHT_MIN_RANGE = .5;
 
     public void init (){
         fright = hardwareMap.dcMotor.get("fright");
         bright = hardwareMap.dcMotor.get("bright");
         fleft = hardwareMap.dcMotor.get("fleft");
         bleft = hardwareMap.dcMotor.get("bleft");
-       // sponge=hardwareMap.dcMotor.get("sponge");
+        sponge=hardwareMap.dcMotor.get("sponge");
         left= hardwareMap.servo.get("left");
         right=hardwareMap.servo.get("right");
         belt=hardwareMap.servo.get("belt");
         camleft=hardwareMap.servo.get("camleft");
-        camright=hardwareMap.servo.get("camright");
+        //camright=hardwareMap.servo.get("camright");
         leftPosition=.9;
         rightPosition=.1;
         beltPosition=.5;
         camleftPosition= 0;
-        camrightPosition=0;
-        camleftPosition= 0; //edit
-        camrightPosition=0; //edit
+        //camrightPosition=.9;
     }
 
 
@@ -68,7 +66,7 @@ public class Prototype extends OpMode {
         bright.setPower(-gamepad1.right_stick_y);
         fleft.setPower(gamepad1.left_stick_y);
         bleft.setPower(gamepad1.left_stick_y);
-        // sponge.setPower(gamepad2.left_stick_y);
+        sponge.setPower(gamepad2.left_stick_y);
         if (gamepad2.dpad_left) {
             leftPosition += leftChange;
         }
@@ -98,24 +96,24 @@ public class Prototype extends OpMode {
             beltPosition=.5;
         }
         if (gamepad2.y){
-            camleftPosition= camleftPosition + .9;
-            camrightPosition=camrightPosition + .9;
+            camleftPosition= camleftPosition - .9;
+            //camrightPosition=camrightPosition + .9;
         }
         if (gamepad2.a){
-            camleftPosition = camleftPosition - camleftChange;
-            camrightPosition= camrightPosition -camleftChange;
+            camleftPosition = camleftPosition + camleftChange;
+            //camrightPosition= camrightPosition -camrightChange;
         }
         leftPosition = Range.clip(leftPosition, LEFT_MIN_RANGE, LEFT_MAX_RANGE);
         rightPosition = Range.clip(rightPosition, RIGHT_MIN_RANGE, RIGHT_MAX_RANGE);
         beltPosition = Range.clip(beltPosition, BELT_MIN_RANGE, BELT_MAX_RANGE);
         camleftPosition = Range.clip (camleftPosition, CAMLEFT_MIN_RANGE, CAMLEFT_MAX_RANGE);
-        camrightPosition = Range.clip (camrightPosition, CAMRIGHT_MIN_RANGE, CAMRIGHT_MAX_RANGE);
+        //camrightPosition = Range.clip (camrightPosition, CAMRIGHT_MIN_RANGE, CAMRIGHT_MAX_RANGE);
 
         left.setPosition(leftPosition);
         right.setPosition(rightPosition);
         belt.setPosition(beltPosition);
         camleft.setPosition (camleftPosition);
-        camright.setPosition(camrightPosition);
+        //camright.setPosition(camrightPosition);
 
         telemetry.addData("S Right: ", gamepad1.right_stick_y);
         telemetry.addData("S Left: ", gamepad1.left_stick_y);
